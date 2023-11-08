5cc2a4e73ff10bea947bdfd7eb6c931ee4eeeb9a
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-11-08 18:18:20 +0900

    fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,25 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
 import About from './About';
 import Projects from './Projects';
-import Contact from './Contact';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
+    <>
+    <Header />
 
+      <Router>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={About} />
+          <Route path="/projects" element={Projects} />
+          <Route path="/contact" element={Contact} />
+      </Router>
+    </>
 
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+      <Home />
+      <Skills />
+      <Background />
+    </>
+  );
+}
+
+export default Home;
