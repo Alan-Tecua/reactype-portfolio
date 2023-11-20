2319ce25e4569455799c1c5a378f36c2c4db6dd4
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-11-21 00:10:55 +0900

    landing logo

--- src/components/landingbanner/LandingBanner.tsx
+++ src/components/landingbanner/LandingBanner.tsx
@@ -7,7 +7,14 @@ const LandingBanner = () => {
       <div className="landingB__contaier container grid">
         <div className="landingB__content grid">
 
-          <div className="landingB__img"></div>
+          <div className="landingB__img">
+          <img
+          src="https://res.cloudinary.com/dd05o0vvo/image/upload/v1700492992/Portfolio/home/Alan-light-logo_lhjxmf.svg"
+          alt="alan_about_img"
+          className="about__img"
+          loading="lazy"
+        />
+          </div>
 
           <LandingInfo/>
         </div>
