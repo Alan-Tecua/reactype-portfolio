87eb7382a51d15d45a5051d099e33e5a2beb6068
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-11-23 14:15:32 +0900

    CSS update

--- src/components/landingbanner/LandingInfo.tsx
+++ src/components/landingbanner/LandingInfo.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom";
 
 const LandingInfo = () => {
   return (
-    <div className="home__data">
-      <h1 className="home__title">Oscar Alan Sanchez Tecuapetla</h1>
+    <div className="landingB__data">
+      <h1 className="landingB__title">Oscar Alan Sanchez Tecuapetla</h1>
       <h3>Fullstack Web Developer + Product Designer</h3>
 
       <NavLink to="/about" className="button button--flex">
