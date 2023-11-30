b60a09574e8cb1b155a1d15d1221df403d1228fe
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-11-30 22:40:52 +0900

    banner css

--- src/components/landingbanner/LandingBanner.tsx
+++ src/components/landingbanner/LandingBanner.tsx
@@ -4,11 +4,9 @@ import LandingInfo from './LandingInfo';
 const LandingBanner = () => {
   return (
     <section className="landingB section" id="landingB">
-      <div className="landingB__contaier container grid">
+      <div className="landingB__container container grid">
         <div className="landingB__content grid">
-
           <div className="landingB__img"></div>
-
           <LandingInfo/>
         </div>
       </div>
