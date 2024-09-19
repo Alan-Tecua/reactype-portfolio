b3ae6a82fe3d1c34cf3e29ad9cdc4dc5ea27eb54
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-09-19 17:53:38 +0900

    HOME page udaptes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import LandingBanner from "../components/landingbanner/LandingBanner";
 import Skills from "../components/skills/Skills";
 import CareerBg from "../components/careerbg/CareerBg";
+import Portfolio from "../components/portfolio/Portfolio";
 
 
 function Home() {
   return (
+    // <>
+    //   <LandingBanner/>
+    //   <Skills />
+    //   <CareerBg />
+    // </>
     <>
-      <LandingBanner/>
-      <Skills />
-      <CareerBg />
+    <Portfolio/>
     </>
   );
 }
